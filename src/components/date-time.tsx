97ffcd453fdca8ecf04e886ec21d2ddcc80079bd
Zeno Leonardi <zenoelioleone@gmail.com>
2024-10-11 23:40:59 +0200

    Embeded skyss time table

--- src/components/date-time.tsx
+++ src/components/date-time.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { onlyTimeHHMM, weekdayAndDate } from "../utils/date";
+import { weekdayAndDate } from "../utils/date";
 
 export default function DateTime() {
   const [currentTime, setCurrentTime] = useState(new Date());
