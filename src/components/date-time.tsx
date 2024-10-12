99d7c9460368d8d2abde1b9c1ad70418c7da30df
Izaak Sarnecki <izaaksarnecki@outlook.com>
2024-10-12 18:47:39 +0200

    add week number and adjust formatting

--- src/components/date-time.tsx
+++ src/components/date-time.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { weekdayAndDate } from "../utils/date";
+import { getWeek } from "date-fns";
 
 export default function DateTime() {
   const [currentTime, setCurrentTime] = useState(new Date());
@@ -23,11 +24,14 @@ export default function DateTime() {
     weekdayAndDate(currentTime).charAt(0).toUpperCase() +
     weekdayAndDate(currentTime).slice(1);
 
+  const weekNumber = getWeek(currentTime);
+
   return (
     <div>
-      <h1 className="text-4xl text-black flex justify-between">
-        <p>{formattedDate}</p>
-        <p>{formattedTime}</p>
+      <h1 className="text-4xl text-black flex justify-between items-center">
+        <p className="text-left flex-1">{formattedDate}</p>
+        <p className="text-center flex-1">Uke: {weekNumber}</p>
+        <p className="text-right flex-1">{formattedTime}</p>
       </h1>
     </div>
   );
