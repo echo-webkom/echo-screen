60aaf6ca856bdd5fa65235c30fbb341e8dffc408
Birk Monsen <birk.monsen@gmail.com>
2024-10-31 12:32:50 +0100

    Add custom timetable feature to screen

---
+++ src/components/entur-timetable.tsx
@@ -0,0 +1,90 @@
+import useUpcomingDepartures from "../hooks/use-upcoming-departures";
+import {
+  extractRouteNumber,
+  formatTime,
+  getTimeDifferenceInMinutes,
+} from "../utils/timetableUtils";
+
+const EnTurTimetable = () => {
+  const { data, loading, error } = useUpcomingDepartures(5000);
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  return (
+    <div className="flex-auto text-center rounded-lg bg-background/70 border-2 shadow-lg overflow-hidden">
+      <div className="bg-muted p-1 w-full font-semibold text-3xl">
+        <h1 className="flex justify-center">
+          Avganger fra {data.stopPlace.name}
+        </h1>
+      </div>
+
+      {data && (
+        <div className="min-w-full">
+          <table className="text-left w-full">
+            <thead className="border-b bg-muted">
+              <tr>
+                <th className="px-5">
+                  <p className="font-semibold">Avgang</p>
+                  <p className="text-xs font-normal">Departure</p>
+                </th>
+                <th className="px-5">
+                  <p className="font-semibold">Linje</p>
+                  <p className="text-xs font-normal">Route</p>
+                </th>
+                <th className="px-5">
+                  <p className="font-semibold">Destinasjon</p>
+                  <p className="text-xs font-normal">Destination</p>
+                </th>
+              </tr>
+            </thead>
+
+            <tbody className="divide-y divide-border">
+              {data.stopPlace.estimatedCalls
+                .filter((call: any) => {
+                  const timeDifference = getTimeDifferenceInMinutes(
+                    call.expectedArrivalTime
+                  );
+                  return timeDifference >= 0;
+                })
+                .map((call: any, index: number) => {
+                  const timeDifference = getTimeDifferenceInMinutes(
+                    call.expectedArrivalTime
+                  );
+
+                  const displayTime =
+                    timeDifference < 15 ||
+                    call.expectedArrivalTime !== call.aimedArrivalTime
+                      ? `${timeDifference} min`
+                      : formatTime(call.expectedArrivalTime);
+
+                  const routeID = extractRouteNumber(
+                    call.serviceJourney.journeyPattern.line.id
+                  );
+
+                  return (
+                    <tr
+                      key={index}
+                      className={index % 2 === 1 ? "bg-muted" : ""}
+                    >
+                      <td className="px-5">{displayTime}</td>
+                      <td className="px-5">{routeID}</td>
+                      <td className="px-5">
+                        {call.destinationDisplay.frontText}
+                      </td>
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </table>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default EnTurTimetable;
---
+++ src/hooks/use-upcoming-departures.ts
@@ -0,0 +1,73 @@
+import { useEffect, useState } from "react";
+
+const useUpcomingDepartures = (interval: number = 5000) => {
+  const [data, setData] = useState<any>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  const fetchData = async () => {
+    const query = `
+    {
+      stopPlace(id: "NSR:StopPlace:58544") {
+        id
+        name
+        estimatedCalls(timeRange: 72100, numberOfDepartures: 10) {
+          realtime
+          aimedArrivalTime
+          aimedDepartureTime
+          expectedArrivalTime
+          expectedDepartureTime
+          destinationDisplay {
+            frontText
+          }
+          quay {
+            id
+          }
+          serviceJourney {
+            journeyPattern {
+              line {
+                id
+              }
+            }
+          }
+        }
+      }
+    }`;
+
+    try {
+      const response = await fetch(
+        "https://api.entur.io/journey-planner/v3/graphql",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "ET-Client-Name": "echo-echoScreen",
+          },
+          body: JSON.stringify({ query }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const result = await response.json();
+      setData(result.data);
+    } catch (error) {
+      setError((error as Error).message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+    const intervalId = setInterval(fetchData, interval);
+
+    return () => clearInterval(intervalId);
+  }, [interval]);
+
+  return { data, loading, error };
+};
+
+export default useUpcomingDepartures;
