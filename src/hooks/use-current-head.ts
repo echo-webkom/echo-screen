060d8aeb542e3cbde41738cfd7b5ec12b1243ee4
Ole Magnus Fon Johnsen <me@omfj.no>
2025-01-29 19:14:45 +0100

    Reload on head change

--- src/hooks/use-current-head.ts
+++ src/hooks/use-current-head.ts
@@ -1,10 +1,11 @@
 import { Octokit } from "@octokit/rest";
 import { useQuery } from "@tanstack/react-query";
 import { useEffect } from "react";
+
 const octokit = new Octokit();
 
 export const currentHead = async () => {
-  octokit.rest.repos
+  return octokit.rest.repos
     .listCommits({
       owner: "echo-webkom",
       repo: "echo-screen",
@@ -13,15 +14,15 @@ export const currentHead = async () => {
 };
 
 export const useReloadUpdate = () => {
-  const { data } = useQuery({
+  const { data: sha } = useQuery({
     queryKey: ["sha"],
     queryFn: currentHead,
     refetchInterval: 1000 * 60 * 5,
   });
 
   useEffect(() => {
-    if (data !== process.env.__COMMIT_HASH__) {
+    if (sha !== import.meta.env.__COMMIT_HASH__) {
       location.reload();
     }
-  }, [data]);
+  }, [sha]);
 };
