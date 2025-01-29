0ae14b407b74120b661143a0ea1377aa3a29106a
Ole Magnus Fon Johnsen <me@omfj.no>
2025-01-29 18:36:36 +0100

    Fix something

--- src/components/bento-screen.tsx
+++ src/components/bento-screen.tsx
@@ -5,42 +5,42 @@ import Calendar from "./calendar";
 import EnTurTimetable from "./entur-timetable";
 import { Bysykkel } from "./bysykkel";
 
-export default function BentoScreen() {
-  const screens = [
-    <div key="screen1" className="w-[100%] h-[100%] flex flex-col gap-7">
-      <Calendar />
-      <BedpressCountDown />
-    </div>,
-    <div key="screen2" className="w-[100%] h-[100%]">
-      <EnTurTimetable />
-      <Bysykkel />
-    </div>,
-  ];
+const SCREENS = [
+  <div key="screen1" className="w-[100%] h-[100%] flex flex-col gap-7">
+    <Calendar />
+    <BedpressCountDown />
+  </div>,
+  <div key="screen2" className="w-[100%] h-[100%]">
+    <EnTurTimetable />
+    <Bysykkel />
+  </div>,
+];
 
+export default function BentoScreen() {
   const [screenIndex, setScreenIndex] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setScreenIndex((prevIndex) => (prevIndex + 1) % screens.length);
+      setScreenIndex((prevIndex) => (prevIndex + 1) % SCREENS.length);
     }, 30000);
 
     return () => clearInterval(interval);
-  }, [screens.length]);
+  }, []);
 
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
       if (event.key === "ArrowRight") {
-        setScreenIndex((prevIndex) => (prevIndex + 1) % screens.length);
+        setScreenIndex((prevIndex) => (prevIndex + 1) % SCREENS.length);
       } else if (event.key === "ArrowLeft") {
         setScreenIndex(
-          (prevIndex) => (prevIndex - 1 + screens.length) % screens.length
+          (prevIndex) => (prevIndex - 1 + SCREENS.length) % SCREENS.length
         );
       }
     };
 
     window.addEventListener("keydown", handleKeyDown);
     return () => window.removeEventListener("keydown", handleKeyDown);
-  }, [screens.length]);
+  }, []);
 
   return (
     <div className="h-[calc(100vh-12rem)] w-[100%] space-y-7">
@@ -54,7 +54,7 @@ export default function BentoScreen() {
             transition={{ duration: 1 }}
             className="w-[100%] h-[100%]"
           >
-            {screens[screenIndex]}
+            {SCREENS[screenIndex]}
           </motion.div>
         </AnimatePresence>
       </div>
