d7d017378b5464bc031b2464d1bda7364e63e495
Zeno Leonardi <zenoelioleone@gmail.com>
2025-01-29 18:41:02 +0100

    Auto refresh

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    __COMMIT_HASH__: JSON.stringify(process.env.CF_PAGES_COMMIT_SHA),
+  },
+});
