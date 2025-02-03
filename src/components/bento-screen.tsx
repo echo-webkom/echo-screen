f57603bf24938442fdebeac770c9e4c711f92cc3
Andreas Drevdal <andreas.drevdal@gmail.com>
2025-02-03 17:13:34 +0100

    created component without birthday functionallity

--- src/components/bento-screen.tsx
+++ src/components/bento-screen.tsx
@@ -4,11 +4,15 @@ import BedpressCountDown from "./bedpress-count-down";
 import Calendar from "./calendar";
 import EnTurTimetable from "./entur-timetable";
 import { Bysykkel } from "./bysykkel";
+import TodaysBirthdays from "./todays-birthdays";
 
 const SCREENS = [
   <div key="screen1" className="w-[100%] h-[100%] flex flex-col gap-7">
     <Calendar />
-    <BedpressCountDown />
+    <div className="flex gap-7">
+      <BedpressCountDown />
+      <TodaysBirthdays />
+    </div>
   </div>,
   <div key="screen2" className="w-[100%] h-[100%]">
     <EnTurTimetable />
---
+++ src/components/todays-birthdays.tsx
@@ -0,0 +1,7 @@
+export default function TodaysBirthdays() {
+  return (
+    <div className="flex gap-10 my-10">
+      <h1>I dag har disse fine menneskene bursdag</h1>
+    </div>
+  );
+}
