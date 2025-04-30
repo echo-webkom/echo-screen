71388bd25eac13abd6a18d943df646c2d66863f2
KaroGil <karolinkagil@gmail.com>
2025-04-30 12:45:54 +0200

    add orakel to frontpage

---
+++ src/components/orakel.tsx
@@ -0,0 +1,28 @@
+export function Orakel() {
+  return (
+    <div className="w-full flex flex-col items-center justify-center bg-background/70 border-2 shadow-lg rounded-lg p-10">
+      <h1 className="text-5xl font-bold drop-shadow-lg">
+        informatikk - Orakel
+      </h1>
+      <p className="mt-4 text-lg max-w-2xl">
+        Få hjelp med infromatikkoppgaver 💻
+      </p>
+      <div className="mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="bg-white/20 p-6 rounded-lg border-2">
+          <h2 className="text-2xl font-semibold">📅 Tid</h2>
+          <p className="mt-2 text-lg font-bold">Onsdag og Torsdag</p>
+          <p className="mt-2 text-lg">14:15 - 16:00</p>
+        </div>
+        <div className="bg-white/20 p-6 rounded-lg border-2">
+          <h2 className="text-2xl font-semibold">📍 Sted</h2>
+          <p className="mt-2 text-lg">Programmerbar</p>
+        </div>
+        <div className="bg-white/20 p-6 rounded-lg border-2">
+          <h2 className="text-2xl font-semibold">🧇 Vaffel</h2>
+          <p className="mt-2 text-lg">Gratis vafler på torsdagene!</p>
+          <p className="mt-2 text-lg italic">Bestilles gjennom discord</p>
+        </div>
+      </div>
+    </div>
+  );
+}
