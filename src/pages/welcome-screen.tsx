4f9d31eb8be864b0a7767fcbf66b61b4ad54a528
Ole Magnus Fon Johnsen <me@omfj.no>
2025-08-05 18:05:11 +0200

    Smaller QR code

--- src/pages/welcome-screen.tsx
+++ src/pages/welcome-screen.tsx
@@ -4,21 +4,24 @@ import echoqr from "../assets/echoqr.png";
 export default function WelcomeScreen() {
   return (
     <>
-      <h1 className="flex text-8xl font-semibold text-center flex-col pt-20">
+      <h1 className="flex text-4xl font-semibold text-center flex-col pt-20">
         Velkommen til lesesalen nye echo-ere!
       </h1>
-      <div className="text-4xl text-center pt-4 text-gray-600">
+
+      <p className="text-2xl text-center max-w-4xl mx-auto text-balance pt-8 text-gray-600">
         Lesesalen er åpen hver dag for alle studenter som er medlem i echo. På lesesalen finner du
         gratis kaffe, programmerbar (åpen hver fredag) og alle dine medstudenter!
-      </div>
-      <div className="flex justify-center gap-48 text-6xl font-semibold pt-16 ">
+      </p>
+
+      <div className="flex justify-center gap-48 text-4xl font-semibold pt-16 ">
         <div className="flex flex-col justify-center items-center ">
-          <h1 className="pb-2">Husk å søke verv!</h1>
-          <img width={580} src={vervqr} className="bg-white/50 border-2 rounded-lg shadow-lg" />
+          <h1 className="pb-4">Husk å søke verv!</h1>
+          <img width={360} src={vervqr} className="bg-white/50 border-2 rounded-lg shadow-lg" />
         </div>
+
         <div className="flex flex-col justify-center items-center ">
-          <h1 className="pb-2">Sjekk ut echo.uib.no!</h1>
-          <img width={580} className="bg-white/50 border-2 rounded-lg shadow-lg" src={echoqr} />
+          <h1 className="pb-4">Sjekk ut echo.uib.no!</h1>
+          <img width={360} className="bg-white/50 border-2 rounded-lg shadow-lg" src={echoqr} />
         </div>
       </div>
     </>
