49e24562fa3917299f0483ad883cc1c689d87175
mrclina <marcelinadylwska@gmail.com>
2025-10-22 17:11:33 +0200

    bday countdown (#58)
    * bday countdown
    
    * calendar screen

--- src/pages/calendar-screen.tsx
+++ src/pages/calendar-screen.tsx
@@ -1,4 +1,4 @@
-import BedpressCountDown from "../components/bedpress-count-down";
+import BdayCountDown from "../components/birthday-countdown";
 import Calendar from "../components/calendar";
 import { Orakel } from "../components/orakel";
 import TodaysBirthdays from "../components/todays-birthdays";
@@ -8,7 +8,7 @@ export const CalendarScreen = () => {
     <div className="w-full h-full flex flex-col gap-7">
       <Calendar />
       <div className="flex gap-7">
-        <BedpressCountDown />
+        <BdayCountDown />
         <Orakel />
         <TodaysBirthdays />
       </div>
