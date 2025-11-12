a4d60214f2bc1b6fccbb6c8a7a74d89a48435fd6
Herwal <herwal20@gmail.com>
2025-11-12 19:38:56 +0100

    format, lint, og refactor

--- src/components/weather.tsx
+++ src/components/weather.tsx
@@ -1,72 +1,93 @@
 import { useWeather } from "../hooks/use-weather";
-import { TiWeatherDownpour, TiWeatherCloudy, TiWeatherSnow, TiWeatherSunny, TiWeatherWindy } from "react-icons/ti";
+import {
+  TiWeatherDownpour,
+  TiWeatherCloudy,
+  TiWeatherSnow,
+  TiWeatherSunny,
+  TiWeatherWindy
+} from "react-icons/ti";
 import { FaTemperatureLow } from "react-icons/fa";
 import { FaTemperatureHalf, FaTemperatureFull } from "react-icons/fa6";
 
 export function Weather() {
-    const {data: weather} = useWeather();
+  const { data: weather } = useWeather();
 
-    if (!weather) { return <p>Ingen værdata tilgjengelig</p>; }
+  if (!weather) {
+    return <p>Ingen værdata tilgjengelig</p>;
+  }
 
-    if (!weather.temperature) { return <p>Laster værdata...</p>; }
-    if (!weather.wind_speed) { return <p>Laster vinddata...</p>; }
-    if (!weather.condition) { return <p>Laster skydata...</p>; }
+  if (!weather.temperature) {
+    return <p>Laster værdata...</p>;
+  }
+  if (!weather.wind_speed) {
+    return <p>Laster vinddata...</p>;
+  }
+  if (!weather.condition) {
+    return <p>Laster skydata...</p>;
+  }
 
-    let WeatherIcon;
-    let cond;
-    switch (weather.condition) {
-        case "rainy":
-            WeatherIcon = TiWeatherDownpour;
-            cond = "regn"
-            break;
-        case "cloudy":
-            WeatherIcon = TiWeatherCloudy;
-            cond = "skyet"
-            break;
-        case "snowy":
-            WeatherIcon = TiWeatherSnow;
-            cond = "snø"
-            break;
-        case "sunny":
-            WeatherIcon = TiWeatherSunny;
-            cond = "sol"
-            break;
-        default:
-            WeatherIcon = null;
-            cond = null;
-    }
+  const { WeatherIcon, norCond } = getIconForCondition(weather.condition);
+  const { TempIcon, temp } = getTempIcon(weather.temperature);
 
-    let TemperatureIcon;
-    const temp = parseFloat(weather.temperature);
-    if (temp <= 0) {
-        TemperatureIcon = FaTemperatureLow;
-    }
-    else if (temp > 0 && temp < 15) {
-        TemperatureIcon = FaTemperatureHalf;
-    }
-    else if (temp >= 15) {
-        TemperatureIcon = FaTemperatureFull;
-    } else {
-        TemperatureIcon = null;
-    }
+  return (
+    <div className="flex items-center gap-4 text-md text-gray-700">
+      <div className="flex items-center gap-1">
+        <span>{temp}°C</span>
+        {TempIcon && <TempIcon className="text-md opacity-80" />}
+      </div>
 
-    return (
-        <div className="flex items-center gap-4 text-md text-gray-700">
-            <div className="flex items-center gap-1">
-                <span>{weather.temperature}°C</span>
-                {TemperatureIcon && <TemperatureIcon className="text-md opacity-80" />}
-            </div>
+      <div className="flex items-center gap-1">
+        <span>{norCond}</span>
+        {WeatherIcon && <WeatherIcon className="text-md opacity-80" />}
+      </div>
 
-            <div className="flex items-center gap-1">
-                <span>{cond}</span>
-                {WeatherIcon && <WeatherIcon className="text-md opacity-80" />}
-            </div>
+      <div className="flex items-center gap-1">
+        <span>{weather.wind_speed} m/s</span>
+        <TiWeatherWindy className="text-md opacity-80" />
+      </div>
+    </div>
+  );
+}
 
-            <div className="flex items-center gap-1">
-                <span>{weather.wind_speed} m/s</span>
-                <TiWeatherWindy className="text-md opacity-80" />
-            </div>
-        </div>
-    );
+function getTempIcon(temperature: string) {
+  let TempIcon;
+  const temp = parseFloat(temperature);
 
+  if (temp <= 0) {
+    TempIcon = FaTemperatureLow;
+  } else if (temp > 0 && temp < 15) {
+    TempIcon = FaTemperatureHalf;
+  } else if (temp >= 15) {
+    TempIcon = FaTemperatureFull;
+  } else {
+    TempIcon = null;
+  }
+  return { TempIcon, temp };
+}
+
+function getIconForCondition(condition: string) {
+  let WeatherIcon;
+  let norCond;
+  switch (condition) {
+    case "rainy":
+      WeatherIcon = TiWeatherDownpour;
+      norCond = "regn";
+      break;
+    case "cloudy":
+      WeatherIcon = TiWeatherCloudy;
+      norCond = "skyet";
+      break;
+    case "snowy":
+      WeatherIcon = TiWeatherSnow;
+      norCond = "snø";
+      break;
+    case "sunny":
+      WeatherIcon = TiWeatherSunny;
+      norCond = "sol";
+      break;
+    default:
+      WeatherIcon = null;
+      norCond = null;
+  }
+  return { WeatherIcon, norCond };
 }
--- src/hooks/use-weather.ts
+++ src/hooks/use-weather.ts
@@ -1,20 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
 
-const API_URL = "https://weather.echo-webkom.no/weather"
+const API_URL = "https://weather.echo-webkom.no/weather";
 export type WeatherData = {
-    temperature: string;
-    condition: string;
-    wind_speed: string;
+  temperature: string;
+  condition: string;
+  wind_speed: string;
 };
 
 export const useWeather = () => {
   return useQuery({
     queryKey: ["weather"],
     queryFn: async () => {
-      const weather = await fetch(API_URL).then(
-        (res) => res.json() as Promise<WeatherData>
-      );
-      return weather; 
+      const weather = await fetch(API_URL).then((res) => res.json() as Promise<WeatherData>);
+      return weather;
     }
   });
 };
