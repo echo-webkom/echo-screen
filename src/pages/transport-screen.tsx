2f87c1e9298b66bd361d91fe2de8f756bb52f7f9
mrclina <marcelinadylwska@gmail.com>
2025-11-12 17:42:12 +0100

    Marcelina/snow animation (#62)
    * snow animation screen
    
    * calendar screen
    
    * snow animation
    
    * snow animation

--- src/pages/transport-screen.tsx
+++ src/pages/transport-screen.tsx
@@ -1,11 +1,13 @@
 import { Bysykkel } from "../components/bysykkel";
 import EnTurTimetable from "../components/entur-timetable";
+import SnowMinimal from "../components/christmas-decoration";
 
 export const TransportScreen = () => {
   return (
     <div className="w-full h-full">
       <EnTurTimetable />
       <Bysykkel />
+      <SnowMinimal />
     </div>
   );
 };
