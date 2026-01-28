5d477fa9eac8e8cf9766470c0cc2134d23771e90
mrclina <marcelinadylwska@gmail.com>
2026-01-28 16:59:13 +0100

    Remove snow (#76)
    * remove snow
    
    * remove snow
    
    ---------
    
    Co-authored-by: Marcelina <madyl4861@uib.no>

--- src/pages/transport-screen.tsx
+++ src/pages/transport-screen.tsx
@@ -1,13 +1,12 @@
 import { Bysykkel } from "../components/bysykkel";
 import EnTurTimetable from "../components/entur-timetable";
-import SnowMinimal from "../components/christmas-decoration";
+// import SnowMinimal from "../components/christmas-decoration";
 
 export const TransportScreen = () => {
   return (
     <div className="w-full h-full">
       <EnTurTimetable />
       <Bysykkel />
-      <SnowMinimal />
     </div>
   );
 };
